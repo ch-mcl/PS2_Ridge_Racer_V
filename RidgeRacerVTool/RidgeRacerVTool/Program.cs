331d18553e6792509a7ee2f0788935cbf84c17ba
ch-mcl <mphchmcl@gmail.com>
2025-08-07 06:41:20 +0900

    initial commit for cmd tool

---
+++ RidgeRacerVTool/RidgeRacerVTool/Program.cs
@@ -0,0 +1,69 @@
+﻿using CommandLine;
+using RidgeRacerVTool.TOC;
+using System;
+using System.IO;
+
+namespace RidgeRacerVTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Parser.Default.ParseArguments<UnpackVerbs>(args)
+                .WithParsed<UnpackVerbs>(Unpack);
+        }
+
+        public static void Unpack(UnpackVerbs options)
+        {
+            if (!File.Exists(options.ElfPath))
+            {
+                Console.WriteLine($"Provided ELF file '{options.ElfPath}' does not exist.");
+                return;
+            }
+
+            if (!File.Exists(options.InputPath))
+            {
+                Console.WriteLine($"Provided R5.ALL or RRV_1 file '{options.InputPath}' does not exist.");
+                return;
+            }
+
+            string elfName = Path.GetFileName(options.ElfPath);
+
+            var toc = new TableOfContents(elfName, options.ElfPath);
+            //toc.
+
+            return;
+        }
+    }
+
+    [Verb("unpack", HelpText = "Unpacks R5.ALL(PS2) / RRV1_A(SYSTEM246)")]
+    public class UnpackVerbs
+    {
+        [Option('i', "input", Required = true, HelpText = "Input .DAT file like R5.ALL.")]
+        public string InputPath { get; set; }
+
+        [Option('e', "elf-path", Required = true, HelpText = "Input elf file. Example: SLUS_200.02.")]
+        public string ElfPath { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Output directory for the extracted files.")]
+        public string OutputPath { get; set; }
+
+    }
+
+    //[Verb("pack", HelpText = "Packs R5.ALL(PS2) / RRV1_A(SYSTEM246). Also patching elf file.")]
+    //public class PackArchiveVerbs
+    //{
+    //    [Option('i', "input", Required = true, HelpText = "Input .DAT file like RR7.DAT.")]
+    //    public string InputPath { get; set; }
+
+    //    [Option('e', "elf-path", Required = true, HelpText = "Input elf file. Example: SLUS_200.02.")]
+    //    public string ElfPath { get; set; }
+
+    //    [Option('o', "output", Required = true, HelpText = "Output directory for the packed files.")]
+    //    public string OutputPath { get; set; }
+
+    //}
+
+}
---
+++ RidgeRacerVTool/RidgeRacerVTool/TOC/TableOfContents.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Syroot.BinaryData;
+
+namespace RidgeRacerVTool.TOC
+{
+    partial record TOCInformation (int FileCount, int TocAddress, string ArcName);
+
+    class TableOfContents
+    {
+        public static Dictionary<string, TOCInformation> TOCInfos = new()
+        {
+            { "SLPS_200.01",    new TOCInformation(1136, 0x10BFE8, "R5.ALL") }, // JP
+            { "SLUS_200.02",    new TOCInformation(1136, 0x10D258, "R5.ALL") }, // US
+            { "SCES_500.00",    new TOCInformation(1208, 0x1103B8, "R5.ALL") }, // EU
+            { "rrv3vera.ic002", new TOCInformation(1155, 0x1AB398, "RRV1_A") }, // AC_RRV3_A (Arcade Battle RRV3 Ver A)
+            { "SLPM_601.09",    new TOCInformation(1320,  0xFD2E0, "R5.ALL") }  // JP (Demo, Taikenban)
+        };
+
+        public TOCInformation CurrentTOCInfo { get; set; }
+
+
+//      public List<RRFileDescriptor> FileDescriptors = new();
+//        public List<RRContainerDescriptor> ContainerDescriptors = new();
+
+        private string _elfPath;
+
+        public TableOfContents(string elfName, string elfPath)
+        {
+            if (!TOCInfos.TryGetValue(elfName, out TOCInformation toc))
+                throw new ArgumentException("Invalid or non-supported game code provided.");
+
+            CurrentTOCInfo = toc;
+            _elfPath = elfPath;
+        }
+
+        public void Parse()
+        {
+            using var fileStream = new FileStream(_elfPath, FileMode.Open);
+            using var binaryStream = new BinaryStream(fileStream, ByteConverter.Little);
+
+            fileStream.Position = CurrentTOCInfo.TocAddress;
+
+        }
+    }
+}
